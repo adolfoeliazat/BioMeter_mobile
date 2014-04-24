310b9b3294ded8a0bdeaea9735d1ceccb91a3bfa
Alan Grizek <alangrizek@gmail.com>
2014-04-23 22:34:07 -0300

    Initial commit is now working on Eclipse

--- BioMeter/src/com/itba/biometer/MainActivity.java
+++ BioMeter/src/com/itba/biometer/MainActivity.java
@@ -1,13 +1,15 @@
 package com.itba.biometer;
-import android.os.Bundle;
+
+import java.util.Set;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import java.util.Set;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
@@ -15,6 +17,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class MainActivity extends Activity {
 
